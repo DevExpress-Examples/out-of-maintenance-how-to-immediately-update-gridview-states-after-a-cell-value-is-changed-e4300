f01ea186fb0c84ffbd329d0c193c0e57c8848c98
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.colorEdit1 = new DevExpress.XtraEditors.ColorEdit();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.colorEdit2 = new DevExpress.XtraEditors.ColorEdit();
+            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Location = new System.Drawing.Point(12, 12);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(722, 444);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsSelection.EnableAppearanceFocusedRow = false;
+            this.gridView1.RowStyle += new DevExpress.XtraGrid.Views.Grid.RowStyleEventHandler(this.gridView1_RowStyle);
+            this.gridView1.CustomSummaryCalculate += new DevExpress.Data.CustomSummaryEventHandler(this.gridView1_CustomSummaryCalculate);
+            this.gridView1.CustomUnboundColumnData += new DevExpress.XtraGrid.Views.Base.CustomColumnDataEventHandler(this.gridView1_CustomUnboundColumnData);
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.colorEdit2);
+            this.layoutControl1.Controls.Add(this.colorEdit1);
+            this.layoutControl1.Controls.Add(this.gridControl1);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.layoutControlGroup1;
+            this.layoutControl1.Size = new System.Drawing.Size(746, 516);
+            this.layoutControl1.TabIndex = 1;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.CustomizationFormText = "layoutControlGroup1";
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1,
+            this.layoutControlItem2,
+            this.layoutControlItem3});
+            this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlGroup1.Name = "layoutControlGroup1";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(746, 516);
+            this.layoutControlGroup1.Text = "layoutControlGroup1";
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.gridControl1;
+            this.layoutControlItem1.CustomizationFormText = "layoutControlItem1";
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(726, 448);
+            this.layoutControlItem1.Text = "layoutControlItem1";
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextToControlDistance = 0;
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // colorEdit1
+            // 
+            this.colorEdit1.EditValue = System.Drawing.Color.Empty;
+            this.colorEdit1.Location = new System.Drawing.Point(426, 460);
+            this.colorEdit1.Name = "colorEdit1";
+            this.colorEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.colorEdit1.Size = new System.Drawing.Size(308, 20);
+            this.colorEdit1.StyleController = this.layoutControl1;
+            this.colorEdit1.TabIndex = 4;
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.colorEdit1;
+            this.layoutControlItem2.CustomizationFormText = "The row\'s back color where the RowSum values are greater than 50 but less than 10" +
+    "0";
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 448);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(726, 24);
+            this.layoutControlItem2.Text = "The row\'s back color in which the RowSum values is greater than 50 but less than " +
+    "100";
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(410, 13);
+            // 
+            // colorEdit2
+            // 
+            this.colorEdit2.EditValue = System.Drawing.Color.Empty;
+            this.colorEdit2.Location = new System.Drawing.Point(426, 484);
+            this.colorEdit2.Name = "colorEdit2";
+            this.colorEdit2.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.colorEdit2.Size = new System.Drawing.Size(308, 20);
+            this.colorEdit2.StyleController = this.layoutControl1;
+            this.colorEdit2.TabIndex = 5;
+            // 
+            // layoutControlItem3
+            // 
+            this.layoutControlItem3.Control = this.colorEdit2;
+            this.layoutControlItem3.CustomizationFormText = "The row\'s back color where the RowSum values are greater than 100";
+            this.layoutControlItem3.Location = new System.Drawing.Point(0, 472);
+            this.layoutControlItem3.Name = "layoutControlItem3";
+            this.layoutControlItem3.Size = new System.Drawing.Size(726, 24);
+            this.layoutControlItem3.Text = "The row\'s back color in which the RowSum values is greater than 100";
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(410, 13);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 516);
+            this.Controls.Add(this.layoutControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraEditors.ColorEdit colorEdit2;
+        private DevExpress.XtraEditors.ColorEdit colorEdit1;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+    }
+}
+
---
+++ VB/WindowsFormsApplication1/Form1.Designer.vb
@@ -0,0 +1,180 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Namespace WindowsFormsApplication1
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+			Me.layoutControl1 = New DevExpress.XtraLayout.LayoutControl()
+			Me.layoutControlGroup1 = New DevExpress.XtraLayout.LayoutControlGroup()
+			Me.layoutControlItem1 = New DevExpress.XtraLayout.LayoutControlItem()
+			Me.colorEdit1 = New DevExpress.XtraEditors.ColorEdit()
+			Me.layoutControlItem2 = New DevExpress.XtraLayout.LayoutControlItem()
+			Me.colorEdit2 = New DevExpress.XtraEditors.ColorEdit()
+			Me.layoutControlItem3 = New DevExpress.XtraLayout.LayoutControlItem()
+			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.layoutControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.layoutControl1.SuspendLayout()
+			CType(Me.layoutControlGroup1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.layoutControlItem1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.colorEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.layoutControlItem2, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.colorEdit2.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.layoutControlItem3, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' gridControl1
+			' 
+			Me.gridControl1.Location = New System.Drawing.Point(12, 12)
+			Me.gridControl1.MainView = Me.gridView1
+			Me.gridControl1.Name = "gridControl1"
+			Me.gridControl1.Size = New System.Drawing.Size(722, 444)
+			Me.gridControl1.TabIndex = 0
+			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
+			' 
+			' gridView1
+			' 
+			Me.gridView1.GridControl = Me.gridControl1
+			Me.gridView1.Name = "gridView1"
+			Me.gridView1.OptionsSelection.EnableAppearanceFocusedRow = False
+'			Me.gridView1.RowStyle += New DevExpress.XtraGrid.Views.Grid.RowStyleEventHandler(Me.gridView1_RowStyle);
+'			Me.gridView1.CustomSummaryCalculate += New DevExpress.Data.CustomSummaryEventHandler(Me.gridView1_CustomSummaryCalculate);
+'			Me.gridView1.CustomUnboundColumnData += New DevExpress.XtraGrid.Views.Base.CustomColumnDataEventHandler(Me.gridView1_CustomUnboundColumnData);
+			' 
+			' layoutControl1
+			' 
+			Me.layoutControl1.Controls.Add(Me.colorEdit2)
+			Me.layoutControl1.Controls.Add(Me.colorEdit1)
+			Me.layoutControl1.Controls.Add(Me.gridControl1)
+			Me.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.layoutControl1.Location = New System.Drawing.Point(0, 0)
+			Me.layoutControl1.Name = "layoutControl1"
+			Me.layoutControl1.Root = Me.layoutControlGroup1
+			Me.layoutControl1.Size = New System.Drawing.Size(746, 516)
+			Me.layoutControl1.TabIndex = 1
+			Me.layoutControl1.Text = "layoutControl1"
+			' 
+			' layoutControlGroup1
+			' 
+			Me.layoutControlGroup1.CustomizationFormText = "layoutControlGroup1"
+			Me.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True
+			Me.layoutControlGroup1.GroupBordersVisible = False
+			Me.layoutControlGroup1.Items.AddRange(New DevExpress.XtraLayout.BaseLayoutItem() { Me.layoutControlItem1, Me.layoutControlItem2, Me.layoutControlItem3})
+			Me.layoutControlGroup1.Location = New System.Drawing.Point(0, 0)
+			Me.layoutControlGroup1.Name = "layoutControlGroup1"
+			Me.layoutControlGroup1.Size = New System.Drawing.Size(746, 516)
+			Me.layoutControlGroup1.Text = "layoutControlGroup1"
+			Me.layoutControlGroup1.TextVisible = False
+			' 
+			' layoutControlItem1
+			' 
+			Me.layoutControlItem1.Control = Me.gridControl1
+			Me.layoutControlItem1.CustomizationFormText = "layoutControlItem1"
+			Me.layoutControlItem1.Location = New System.Drawing.Point(0, 0)
+			Me.layoutControlItem1.Name = "layoutControlItem1"
+			Me.layoutControlItem1.Size = New System.Drawing.Size(726, 448)
+			Me.layoutControlItem1.Text = "layoutControlItem1"
+			Me.layoutControlItem1.TextSize = New System.Drawing.Size(0, 0)
+			Me.layoutControlItem1.TextToControlDistance = 0
+			Me.layoutControlItem1.TextVisible = False
+			' 
+			' colorEdit1
+			' 
+			Me.colorEdit1.EditValue = System.Drawing.Color.Empty
+			Me.colorEdit1.Location = New System.Drawing.Point(426, 460)
+			Me.colorEdit1.Name = "colorEdit1"
+			Me.colorEdit1.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() { New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+			Me.colorEdit1.Size = New System.Drawing.Size(308, 20)
+			Me.colorEdit1.StyleController = Me.layoutControl1
+			Me.colorEdit1.TabIndex = 4
+			' 
+			' layoutControlItem2
+			' 
+			Me.layoutControlItem2.Control = Me.colorEdit1
+			Me.layoutControlItem2.CustomizationFormText = "The row's back color where the RowSum values are greater than 50 but less than 10" & "0"
+			Me.layoutControlItem2.Location = New System.Drawing.Point(0, 448)
+			Me.layoutControlItem2.Name = "layoutControlItem2"
+			Me.layoutControlItem2.Size = New System.Drawing.Size(726, 24)
+			Me.layoutControlItem2.Text = "The row's back color in which the RowSum values is greater than 50 but less than " & "100"
+			Me.layoutControlItem2.TextSize = New System.Drawing.Size(410, 13)
+			' 
+			' colorEdit2
+			' 
+			Me.colorEdit2.EditValue = System.Drawing.Color.Empty
+			Me.colorEdit2.Location = New System.Drawing.Point(426, 484)
+			Me.colorEdit2.Name = "colorEdit2"
+			Me.colorEdit2.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() { New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+			Me.colorEdit2.Size = New System.Drawing.Size(308, 20)
+			Me.colorEdit2.StyleController = Me.layoutControl1
+			Me.colorEdit2.TabIndex = 5
+			' 
+			' layoutControlItem3
+			' 
+			Me.layoutControlItem3.Control = Me.colorEdit2
+			Me.layoutControlItem3.CustomizationFormText = "The row's back color where the RowSum values are greater than 100"
+			Me.layoutControlItem3.Location = New System.Drawing.Point(0, 472)
+			Me.layoutControlItem3.Name = "layoutControlItem3"
+			Me.layoutControlItem3.Size = New System.Drawing.Size(726, 24)
+			Me.layoutControlItem3.Text = "The row's back color in which the RowSum values is greater than 100"
+			Me.layoutControlItem3.TextSize = New System.Drawing.Size(410, 13)
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(746, 516)
+			Me.Controls.Add(Me.layoutControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+'			Me.Load += New System.EventHandler(Me.Form1_Load);
+			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.layoutControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.layoutControl1.ResumeLayout(False)
+			CType(Me.layoutControlGroup1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.layoutControlItem1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.colorEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.layoutControlItem2, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.colorEdit2.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.layoutControlItem3, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private gridControl1 As DevExpress.XtraGrid.GridControl
+		Private WithEvents gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+		Private layoutControl1 As DevExpress.XtraLayout.LayoutControl
+		Private colorEdit2 As DevExpress.XtraEditors.ColorEdit
+		Private colorEdit1 As DevExpress.XtraEditors.ColorEdit
+		Private layoutControlGroup1 As DevExpress.XtraLayout.LayoutControlGroup
+		Private layoutControlItem1 As DevExpress.XtraLayout.LayoutControlItem
+		Private layoutControlItem2 As DevExpress.XtraLayout.LayoutControlItem
+		Private layoutControlItem3 As DevExpress.XtraLayout.LayoutControlItem
+	End Class
+End Namespace
+
